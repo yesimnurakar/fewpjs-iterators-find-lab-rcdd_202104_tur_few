98ab24400f14f4945e05918901da47a095404016
Yesim nur Akar <yesimnurakar@gmail.com>
2021-05-15 14:27:53 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+function superbowlWin(record){
+  let winningYear;
+  let lostGame;
+record.find( function(recordArray){
+  if(recordArray.result === "W") {
+    winningYear = recordArray.year;
+  }
+  else {
+    lostGame = undefined;
+    return lostGame;
+  }
+})
+return winningYear;
+};
+superbowlWin(record);
